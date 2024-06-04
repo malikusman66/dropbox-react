6a4aca69556a0a578ef59824519b06e1f0535d4e
malikusman66 <usmannawaz0304@gmail.com>
2024-06-04 19:23:21 +0500

    add component createDropdown

--- src/components/CreateDropdown.js
+++ src/components/CreateDropdown.js
@@ -1,7 +1,56 @@
 import React, { useState } from 'react';
 import './CreateDropdown.css';
-import { FaFolder, FaFileAlt, FaChartBar, FaFileExcel, FaLink, FaUpload, FaFileSignature } from 'react-icons/fa';
+import { FaFolder, FaFileAlt, FaChartBar, FaFileExcel, FaLink, FaUpload, FaFileSignature, FaPen } from 'react-icons/fa';
 import { MdDriveFileMove } from 'react-icons/md';
+
+const menuItems = [
+  {
+    title: 'Folder',
+    icon: <FaFolder />,
+    subMenu: ['Folder', 'Shared folder', 'Automated folder']
+  },
+  {
+    title: 'Document',
+    icon: <FaFileAlt />,
+    subMenu: ['Dropbox Paper doc', 'Dropbox Paper template', 'Microsoft Word', 'Google Docs']
+  },
+  {
+    title: 'Presentation',
+    icon: <FaChartBar />,
+    subMenu: ['Google Slides', 'Microsoft PowerPoint']
+  },
+  {
+    title: 'Spreadsheet',
+    icon: <FaFileExcel />,
+    subMenu: ['Google Sheets', 'Microsoft Excel']
+  },
+  {
+    title: 'Web shortcut',
+    icon: <FaLink />
+  },
+  {
+    title: 'Upload',
+    icon: <FaUpload />
+  },
+  {
+    title: 'Send file request',
+    icon: <FaFileSignature />
+  },
+  {
+    title: 'Import from Google Drive',
+    icon: <MdDriveFileMove />
+  },
+  {
+    title: 'Edit',
+    icon: <FaPen />,
+    subMenu: ['Edit PDF', 'Edit Image']
+  },
+  {
+    title: 'Sign',
+    icon: <FaPen />,
+    subMenu: ['Sign Document']
+  }
+];
 
 const CreateDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,38 +64,20 @@ const CreateDropdown = () => {
       <button className="dropdown-btn" onClick={toggleDropdown}>Create</button>
       {isOpen && (
         <div className="dropdown-content">
-          <div className="dropdown-item" onClick={() => toggleSubMenu('folder')}>
-            <FaFolder /> Folder
-          </div>
-          <div className="dropdown-item" onClick={() => toggleSubMenu('document')}>
-            <FaFileAlt /> Document
-            {subMenu === 'document' && (
-              <div className="sub-menu">
-                <div className="sub-menu-item">Dropbox Paper doc</div>
-                <div className="sub-menu-item">Dropbox Paper template</div>
-                <div className="sub-menu-item">Microsoft Word</div>
-                <div className="sub-menu-item">Google Docs</div>
-              </div>
-            )}
-          </div>
-          <div className="dropdown-item">
-            <FaChartBar /> Presentation
-          </div>
-          <div className="dropdown-item">
-            <FaFileExcel /> Spreadsheet
-          </div>
-          <div className="dropdown-item">
-            <FaLink /> Web shortcut
-          </div>
-          <div className="dropdown-item">
-            <FaUpload /> Upload
-          </div>
-          <div className="dropdown-item">
-            <FaFileSignature /> Send file request
-          </div>
-          <div className="dropdown-item">
-            <MdDriveFileMove /> Import from Google Drive
-          </div>
+          {menuItems.map((item, index) => (
+            <div key={index} className="dropdown-item" onClick={() => item.subMenu && toggleSubMenu(item.title)}>
+              {item.icon} {item.title}
+              {subMenu === item.title && (
+                <div className="sub-menu">
+                  {item.subMenu.map((subItem, subIndex) => (
+                    <div key={subIndex} className="sub-menu-item">
+                      {subItem}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       )}
     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css';
+import { FaFolder, FaImages, FaSign, FaBars } from 'react-icons/fa';
 
 const Sidebar = () => {
+  const [activeMenu, setActiveMenu] = useState(null);
+
+  const toggleMenu = (menu) => {
+    if (activeMenu === menu) {
+      setActiveMenu(null);
+    } else {
+      setActiveMenu(menu);
+    }
+  };
+
   return (
-  
-    <aside className="sidebar">
-      <nav>
-        <ul>
-          <li>All files</li>
-          <li>Photos</li>
-          <li>Shared</li>
-          <li>Signatures</li>
-          <li>File requests</li>
-          <li>Deleted files</li>
-        </ul>
-      </nav>
-      <div className="footer">
-        <button>Get started</button>
+    <div className="sidebar">
+      <div className="sidebar-item" onClick={() => toggleMenu('allFiles')}>
+        <FaBars /> All Files
       </div>
-    </aside>
+      <div className="sidebar-item" onClick={() => toggleMenu('photos')}>
+        <FaImages /> Photos
+        {activeMenu === 'photos' && (
+          <div className="submenu">
+            <div className="submenu-item">Albums</div>
+            <div className="submenu-item">Tags</div>
+          </div>
+        )}
+      </div>
+      <div className="sidebar-item" onClick={() => toggleMenu('shared')}>
+        <FaFolder /> Shared
+        {activeMenu === 'shared' && (
+          <div className="submenu">
+            <div className="submenu-item">Folders</div>
+            <div className="submenu-item">Files</div>
+          </div>
+        )}
+      </div>
+      <div className="sidebar-item" onClick={() => toggleMenu('signatures')}>
+        <FaSign /> Signatures
+        {activeMenu === 'signatures' && (
+          <div className="submenu">
+            <div className="submenu-item">Pending</div>
+            <div className="submenu-item">Completed</div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
